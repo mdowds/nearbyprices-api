12054f3ecb4a70609c3c908c2948d4af152053fc
Matt Dowds <matthewd99@gmail.com>
2016-12-10 20:26:34 +0000

    Changed file loading to make it OS-neutral

--- jsonfromfile/JsonFromFile.py
+++ jsonfromfile/JsonFromFile.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import jsonschema
 from jsonschema import validate
 
@@ -6,23 +7,26 @@ from jsonschema import validate
 class JsonFromFile():
 
     def __init__(self, rootPath, schema):
-        self.rootDataPath = rootPath
+        self.rootDataPath = os.path.join(os.path.dirname(__file__), '..', rootPath)
         self.schema = schema
 
     def getData(self, name):
         try:
             dataFile = open(self.rootDataPath + name + ".json")
         except FileNotFoundError:
+            print("FileNotFoundError")
             return None
 
         try:
             jsonData = json.loads(dataFile.read())
         except json.decoder.JSONDecodeError:
+            print("JSONDecodeError")
             return None
 
         try:
             validate(jsonData, self.schema)
         except jsonschema.exceptions.ValidationError:
+            print("ValidationError")
             return None
 
         return jsonData
