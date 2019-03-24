756ee41735f49b96851b66e2bcb6b7fe82f038c3
Matt Dowds <matthewd99@gmail.com>
2019-03-24 14:48:38 +0000

    Handle postcode not being present in first result from Google Maps

--- position.ts
+++ position.ts
@@ -3,12 +3,18 @@ import {Position, ReverseGeocodingResults} from "./src/types";
 import Firestore = FirebaseFirestore.Firestore;
 import pricesForOutcode from "./src/outcode";
 
+function extractOutcode(results): string {
+    for (const result of results) {
+        const postcode = result.address_components.find(ac => ac.types.includes('postal_code'));
+        if (postcode) return postcode.long_name.split(' ')[0];
+    }
+    return null;
+}
+
 export default async function pricesForPosition(db: Firestore, position: Position) {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.lat},${position.long}&key=${process.env.GMAPS_API_KEY}`;
     const results: ReverseGeocodingResults = await fetch(url).then(r => r.json());
-
-    const postcode = results.results[0].address_components.find(ac => ac.types.includes('postal_code'));
-    const outcode = postcode.long_name.split(' ')[0];
+    const outcode = extractOutcode(results.results);
 
     return await pricesForOutcode(db, outcode);
 }
--- src/outcode.ts
+++ src/outcode.ts
@@ -2,6 +2,7 @@ import Firestore = FirebaseFirestore.Firestore;
 import {PricesForOutcode} from "./types";
 
 export default async function pricesForOutcode(db: Firestore, outcode: string): Promise<PricesForOutcode> {
+    // TODO handle null outcode
     const outcodeRef = db.collection('outcodes').doc(outcode);
 
     const pricesForOutcode = await outcodeRef.get();
